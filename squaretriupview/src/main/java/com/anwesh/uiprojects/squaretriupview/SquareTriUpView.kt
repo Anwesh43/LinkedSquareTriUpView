5f75ac8828b51184b9351fe521bdbce53c77df79
Anwesh43 <anweshthecool0@gmail.com>
2019-04-22 11:05:04 +0530

    adding Canvas extension method drawSTUNode

--- squaretriupview/src/main/java/com/anwesh/uiprojects/squaretriupview/SquareTriUpView.kt
+++ squaretriupview/src/main/java/com/anwesh/uiprojects/squaretriupview/SquareTriUpView.kt
@@ -14,13 +14,13 @@ import android.content.Context
 import android.app.Activity
 
 val nodes : Int = 5
-val squares : Int = 3
+val squares : Int = 2
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
-val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val sFactor : Float = 4.5f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -32,3 +32,33 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
 
+fun Canvas.drawSquareUp(j : Int, sc : Float, gap : Float, size : Float, paint : Paint) {
+    save()
+    translate(gap * sc.divideScale(j, squares), -gap * sc * (1 - 2 * j))
+    drawRect(RectF(-size, -size, size, size), paint)
+    restore()
+}
+
+fun Canvas.drawSTUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val size : Float = gap / sizeFactor
+    val squareSize : Float = size / sFactor
+    paint.color = foreColor
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..1) {
+        save()
+        rotate(180f * j * sc2)
+        translate(-size, 0f)
+        drawRect(RectF(-squareSize, -squareSize, squareSize, squareSize), paint)
+        for (k in 0..(squares - 1)) {
+            drawSquareUp(j, sc1, size, squareSize, paint)
+        }
+        restore()
+    }
+    restore()
+}
