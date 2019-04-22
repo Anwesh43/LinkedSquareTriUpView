b85f0bbea34f2b6432051272218750f2fd5caaf2
Anwesh43 <anweshthecool0@gmail.com>
2019-04-22 11:12:19 +0530

    adding STUNode class

--- squaretriupview/src/main/java/com/anwesh/uiprojects/squaretriupview/SquareTriUpView.kt
+++ squaretriupview/src/main/java/com/anwesh/uiprojects/squaretriupview/SquareTriUpView.kt
@@ -127,4 +127,48 @@ class SquareTriUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class STUNode(var i : Int, val state : State = State()) {
+
+        private var next : STUNode?= null
+        private var prev : STUNode?= null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = STUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSTUNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : STUNode {
+            var curr : STUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
