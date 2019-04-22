76c06dc8b4f06af5dc894d752590068263b66811
Anwesh43 <anweshthecool0@gmail.com>
2019-04-22 10:55:55 +0530

    adding libraries and constants

--- squaretriupview/src/main/java/com/anwesh/uiprojects/squaretriupview/SquareTriUpView.kt
+++ squaretriupview/src/main/java/com/anwesh/uiprojects/squaretriupview/SquareTriUpView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.squaretriupview
 /**
  * Created by anweshmishra on 22/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val squares : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
