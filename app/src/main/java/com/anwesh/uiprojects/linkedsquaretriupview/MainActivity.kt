074b8a80b30d8762bec1eb2b07fc1e3efc22e004
Anwesh43 <anweshthecool0@gmail.com>
2019-04-22 11:17:55 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquaretriupview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquaretriupview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquaretriupview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squaretriupview.SquareTriUpView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareTriUpView.create(this)
     }
 }
